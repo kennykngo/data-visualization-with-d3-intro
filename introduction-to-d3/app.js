8dc2c88962cdcd676993a1e313a01a6f8ef0d901
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-18 23:18:46 -0800

    centering circle of smiley face

--- introduction-to-d3/app.js
+++ introduction-to-d3/app.js
@@ -1,3 +1,16 @@
+// import { select } from "d3";
+
 const svg = d3.select("svg");
 
 svg.style("background-color", "red");
+
+// append method will append new DOM elements
+// will append the element into the svg element
+const circle = svg.append("circle");
+
+const height = svg.height;
+const width = 960;
+
+circle.attr("r", height / 2);
+circle.attr("cx", 960 / 2);
+circle.attr("cy", height / 2);
