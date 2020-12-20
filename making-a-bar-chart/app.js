9d882994c9d16f752787cd6e443fd1fcee69d143
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-20 00:32:46 -0800

    refactored code and added bars to bar chart

--- making-a-bar-chart/app.js
+++ making-a-bar-chart/app.js
@@ -9,9 +9,34 @@ const svg = d3.select("svg");
 const height = +svg.attr("height");
 const width = +svg.attr("width");
 
+const render = (data) => {
+  const xValue = (d) => d.population;
+  const yValue = (d) => d.country;
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, xValue)])
+    .range([0, width]);
+
+  const yScale = d3.scaleBand().domain(data.map(yValue)).range([0, height]);
+
+  console.log(yScale.domain());
+
+  console.log(xScale.range());
+  svg
+    .selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("y", (d) => yScale(yValue(d)))
+    // .append("rect")
+    .attr("width", (d) => xScale(xValue(d)))
+    // .attr("width", (d) => xScale(d.population))
+    .attr("height", yScale.bandwidth());
+};
+
 d3.csv("data.csv").then((data) => {
   data.forEach((d) => {
     d.population = +d.population * 1000;
   });
-  console.log(data);
+  render(data);
 });
