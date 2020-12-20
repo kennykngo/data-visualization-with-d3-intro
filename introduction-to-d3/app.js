e4d0fa4ca52a7b2fa14cab494cc5cc064ceec5c1
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-19 16:30:52 -0800

    updated left and right eye

--- introduction-to-d3/app.js
+++ introduction-to-d3/app.js
@@ -6,16 +6,31 @@ svg.style("background-color", "red");
 
 // append method will append new DOM elements
 // will append the element into the svg element
-const circle = svg.append("circle");
 
 const height = +svg.attr("height");
 const width = +svg.attr("width");
 
-console.log(typeof width);
-
-console.log(svg.attr("width"));
-
-circle
+const circle = svg
+  .append("circle")
   .attr("r", height / 2)
   .attr("cx", width / 2)
-  .attr("cy", height / 2);
+  .attr("cy", height / 2)
+  .attr("fill", "yellow")
+  .attr("stroke", "black");
+
+const eyeSpacing = 100;
+const eyeYOffset = -70;
+
+const leftEye = svg
+  .append("circle")
+  .attr("r", 30)
+  .attr("cx", width / 2 - eyeSpacing)
+  .attr("cy", height / 2 + eyeYOffset)
+  .attr("fill", "black");
+
+const rightEye = svg
+  .append("circle")
+  .attr("r", 30)
+  .attr("cx", width / 2 + eyeSpacing)
+  .attr("cy", height / 2 + eyeYOffset)
+  .attr("fill", "black");
