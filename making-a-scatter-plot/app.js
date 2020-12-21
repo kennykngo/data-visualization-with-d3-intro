9aa28298ff924799bc72ac813b7630742086ac8e
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-20 19:26:37 -0800

    creating a scatter plot

---
+++ making-a-scatter-plot/app.js
@@ -0,0 +1,80 @@
+const svg = d3.select("svg");
+
+// append method will append new DOM elements
+// will append the element into the svg element
+
+const height = +svg.attr("height");
+const width = +svg.attr("width");
+
+const render = (data) => {
+  const xValue = (d) => d.population;
+  const yValue = (d) => d.country;
+  const margin = { top: 50, right: 20, bottom: 70, left: 200 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  const xScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, xValue)])
+    .range([0, innerWidth]);
+
+  const yScale = d3
+    .scaleBand()
+    .domain(data.map(yValue))
+    .range([0, innerHeight])
+    .padding(0.1);
+
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  const xAxisTickFormat = (number) =>
+    d3.format(".3s")(number).replace("G", "B");
+
+  const xAxis = d3
+    .axisBottom(xScale)
+    .tickFormat(xAxisTickFormat)
+    .tickSize(-innerHeight);
+
+  g.append("g")
+    .call(d3.axisLeft(yScale))
+    .selectAll(".domain, .tick line")
+    .remove();
+
+  // bottom axis
+  const xAxisG = g
+    .append("g")
+    .call(xAxis)
+    .attr("transform", `translate(0, ${innerHeight})`);
+
+  xAxisG.select(".domain").remove();
+  // yAxis(g.append("g"));
+
+  xAxisG
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("x", innerWidth / 2)
+    .attr("y", 65)
+    .text("Population")
+    .attr("fill", "black");
+
+  g.selectAll("circle")
+    .data(data)
+    .join("circle")
+    .attr("cy", (d) => yScale(yValue(d)))
+    .attr("cx", (d) => xScale(xValue(d)))
+    // .attr("width", (d) => xScale(d.population))
+    .attr("r", yScale.bandwidth() / 2);
+
+  g.append("text")
+    .attr("y", -10)
+    .text("Top 10 Most Populous Countries")
+    .attr("class", "title");
+};
+
+d3.csv("data.csv").then((data) => {
+  data.forEach((d) => {
+    d.population = +d.population * 1000;
+  });
+  render(data);
+});
