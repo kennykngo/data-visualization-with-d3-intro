cd2b3cc07c050c2353d5572c621c0bd1dcbfcf20
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-21 23:31:31 -0800

    adding an interaction for d3

---
+++ bowl-of-fruit-interaction/app.js
@@ -0,0 +1,31 @@
+// import { range, select } from "d3"
+import { fruitsBowl } from "./fruitBowl.js";
+
+const svg = d3.select("svg");
+
+const makeFruit = (type) => ({ type, id: Math.random() });
+let fruits = d3.range(5).map(() => makeFruit("apple"));
+
+const render = () => {
+  fruitsBowl(svg, { fruits, height: +svg.attr("height") });
+};
+
+render();
+
+setTimeout(() => {
+  fruits.pop();
+
+  render();
+}, 1000);
+
+// replacing an apple with a lemon
+setTimeout(() => {
+  fruits[2].type = "lemon";
+  render();
+}, 2000);
+
+setTimeout(() => {
+  fruits = fruits.filter((d, i) => i !== 1);
+
+  render();
+}, 3000);
---
+++ bowl-of-fruit-interaction/fruitBowl.js
@@ -0,0 +1,36 @@
+const colorScale = d3
+  .scaleOrdinal()
+  .domain(["apple", "lemon"])
+  .range(["#c11d1d", "#eae600"]);
+
+const radiusScale = d3
+  .scaleOrdinal()
+  .domain(["apple", "lemon"])
+  .range([50, 30]);
+
+const xPosition = (d, i) => i * 120 + 60;
+
+export const fruitsBowl = (selection, props) => {
+  const { fruits, height } = props;
+  const circles = selection.selectAll("circle").data(fruits, (d) => d.id);
+
+  circles
+    .enter()
+    .append("circle")
+    .attr("cx", xPosition)
+    .attr("cy", height / 2)
+    .attr("r", 0)
+    .merge(circles)
+    .attr("fill", (d) => colorScale(d.type))
+    .on("click", () => {
+      console.log("clicked");
+    })
+    .transition()
+    .duration(1000)
+    .attr("cx", xPosition)
+    .attr("r", (d) => radiusScale(d.type));
+
+  circles.exit().transition().duration(1000).attr("r", 0).remove();
+};
+
+// module.exports = fruitsBowl;
