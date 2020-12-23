03cf84eee43716318e20a47abea9bb13c61e2f85
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-22 17:28:53 -0800

    createing a world map by using feature from topojson

--- lets-make-a-map/app.js
+++ lets-make-a-map/app.js
@@ -2,15 +2,22 @@
 // import arc from "d3";
 // import { select } from "https://unpkg.com/d3@6.3.1/dist/d3.min.js?module";
 
+// const topojson = require("")
+
 const svg = d3.select("svg");
 
-// append method will append new DOM elements
-// will append the element into the svg element
-
-const height = +svg.attr("height");
-const width = +svg.attr("width");
+const projection = d3.geoMercator();
+const pathGenerator = d3.geoPath().projection(projection);
 
 d3.json("https://unpkg.com/world-atlas@1.1.4/world/110m.json").then((data) => {
   const countries = topojson.feature(data, data.objects.countries);
+  console.log(countries);
   console.log(data);
+
+  svg
+    .selectAll("path")
+    .data(countries.features)
+    .enter()
+    .append("path")
+    .attr("d", (d) => pathGenerator(d));
 });
