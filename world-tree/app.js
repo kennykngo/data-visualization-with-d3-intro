14efb9c205bc2929d56439b8f90b078f190dac46
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-22 23:28:48 -0800

    got world tree working

---
+++ world-tree/app.js
@@ -0,0 +1,24 @@
+const svg = d3.select("svg");
+
+const height = document.body.clientHeight;
+const width = document.body.clientHeight;
+
+const treeLayout = d3.tree().size([height, width]);
+
+svg.attr("width", width).attr("height", height);
+
+d3.json("data.json").then((data) => {
+  const root = d3.hierarchy(data);
+  const links = treeLayout(root).links();
+  const linkPathGenerator = d3
+    .linkHorizontal()
+    .x((d) => d.y)
+    .y((d) => d.x);
+
+  svg
+    .selectAll("path")
+    .data(links)
+    .enter()
+    .append("path")
+    .attr("d", linkPathGenerator);
+});
