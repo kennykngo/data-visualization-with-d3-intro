ab1daa5cc54e6f7affd759e6c71b5b49f781446d
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-22 17:35:44 -0800

    added a sphere around the globe

--- lets-make-a-map/app.js
+++ lets-make-a-map/app.js
@@ -6,18 +6,27 @@
 
 const svg = d3.select("svg");
 
-const projection = d3.geoMercator();
+// manipulate the map by changing the projection
+const projection = d3.geoNaturalEarth1();
 const pathGenerator = d3.geoPath().projection(projection);
+
+// globe path
+svg
+  .append("path")
+  .attr("d", pathGenerator({ type: "Sphere" }))
+  .attr("class", "sphere");
 
 d3.json("https://unpkg.com/world-atlas@1.1.4/world/110m.json").then((data) => {
   const countries = topojson.feature(data, data.objects.countries);
   console.log(countries);
   console.log(data);
 
+  // countries path
   svg
     .selectAll("path")
     .data(countries.features)
     .enter()
     .append("path")
+    .attr("class", "country")
     .attr("d", (d) => pathGenerator(d));
 });
