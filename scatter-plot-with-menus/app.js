450724835e076426711a330f0a95813cbfca335b
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-27 13:11:45 -0800

    updating scatter plot to fill general update pattern

--- scatter-plot-with-menus/app.js
+++ scatter-plot-with-menus/app.js
@@ -1,4 +1,5 @@
 import { dropdownMenu } from "./dropdownMenu.js";
+import { scatterPlot } from "./scatterPlot.js";
 
 const svg = d3.select("svg");
 
@@ -22,89 +23,19 @@ const render = () => {
     onOptionClicked: onXColumnClicked,
   });
 
-  const xValue = (d) => d[xColumn];
-  const xAxisLabel = "Horsepower";
-
-  const yValue = (d) => d.weight;
-  const yAxisLabel = "Weight";
-
-  const title = "Cars: Horsepower vs Weight";
-  const circleRadius = 18;
-  const margin = { top: 60, right: 40, bottom: 88, left: 150 };
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
-
-  const xScale = d3
-    .scaleLinear()
-    // extent returns an array of max and min
-    .domain(d3.extent(data, xValue))
-    .range([0, innerWidth])
-    .nice();
-
-  const yScale = d3
-    .scaleLinear()
-    .domain(d3.extent(data, yValue))
-    .range([0, innerHeight])
-    .nice();
-
-  const g = svg
-    .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-  const xAxisTickFormat = (number) =>
-    d3.format(".3s")(number).replace("G", "B");
-
-  const xAxis = d3
-    .axisBottom(xScale)
-    .tickFormat(xAxisTickFormat)
-    .tickSize(-innerHeight)
-    .tickPadding(15);
-
-  const yAxis = d3
-    .axisLeft(yScale)
-    .tickSize(-innerWidth)
-    .tickPadding(10)
-    .tickFormat(d3.format(".2s"));
-  const yAxisG = g.append("g").call(yAxis);
-
-  yAxisG.selectAll(".domain").remove();
-
-  // bottom axis
-  const xAxisG = g
-    .append("g")
-    .call(xAxis)
-    .attr("transform", `translate(0, ${innerHeight})`);
-
-  xAxisG.select(".domain").remove();
-  // yAxis(g.append("g"));
-
-  yAxisG
-    .append("text")
-    .attr("class", "axis-label")
-    .attr("x", -innerHeight / 2)
-    .attr("y", -93)
-    .attr("transform", `rotate(-90)`)
-    .attr("fill", "black")
-    .attr("text-anchor", "middle")
-    .text(yAxisLabel);
-
-  xAxisG
-    .append("text")
-    .attr("class", "axis-label")
-    .attr("x", innerWidth / 2)
-    .attr("y", 75)
-    .text(xAxisLabel)
-    .attr("fill", "black");
-
-  g.selectAll("circle")
-    .data(data)
-    .join("circle")
-    .attr("cy", (d) => yScale(yValue(d)))
-    .attr("cx", (d) => xScale(xValue(d)))
-    // .attr("width", (d) => xScale(d.population))
-    .attr("r", circleRadius);
-
-  g.append("text").attr("y", -10).text(title).attr("class", "title");
+  //
+  svg.call(scatterPlot, {
+    title: "Cars: Horsepower vs. Weight",
+    xValue: (d) => d[xColumn],
+    xAxisLabel: "Horsepower",
+    yValue: (d) => d.weight,
+    yAxisLabel: "Weight",
+    circleRadius: 18,
+    margin: { top: 60, right: 40, bottom: 88, left: 150 },
+    width,
+    height,
+    data,
+  });
 };
 
 d3.csv("https://vizhub.com/curran/datasets/auto-mpg.csv").then((loadedData) => {
---
+++ scatter-plot-with-menus/scatterPlot.js
@@ -0,0 +1,96 @@
+export const scatterPlot = (selection, props) => {
+  const {
+    title,
+    xValue,
+    xAxisLabel,
+    yValue,
+    yAxisLabel,
+    circleRadius,
+    margin,
+    width,
+    height,
+    data,
+  } = props;
+
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  const xScale = d3
+    .scaleLinear()
+    // extent returns an array of max and min
+    .domain(d3.extent(data, xValue))
+    .range([0, innerWidth])
+    .nice();
+
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, yValue))
+    .range([0, innerHeight])
+    .nice();
+
+  const g = selection.selectAll(".container").data([null]);
+  const gEnter = g.enter().append("g").attr("class", "container");
+  gEnter.merge(g).attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  const xAxisTickFormat = (number) =>
+    d3.format(".3s")(number).replace("G", "B");
+
+  const xAxis = d3
+    .axisBottom(xScale)
+    .tickFormat(xAxisTickFormat)
+    .tickSize(-innerHeight)
+    .tickPadding(15);
+
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickSize(-innerWidth)
+    .tickPadding(10)
+    .tickFormat(d3.format(".2s"));
+
+  const yAxisG = g.select(".y-axis");
+  const yAxisGEnter = gEnter.append("g").attr("class", "y-axis");
+  yAxisG.merge(yAxisGEnter).call(yAxis).selectAll(".domain").remove();
+
+  const yAxisLabelText = yAxisGEnter
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("x", -innerHeight / 2)
+    .attr("y", -93)
+    .attr("transform", `rotate(-90)`)
+    .attr("fill", "black")
+    // update the existing axis-label class
+    .merge(yAxisG.select(".axis-label"))
+    .attr("text-anchor", "middle")
+    .text(yAxisLabel);
+
+  // x axis
+  const xAxisG = g.select(".x-axis");
+  const xAxisGEnter = gEnter.append("g").attr("class", "x-axis");
+  xAxisG
+    .merge(xAxisGEnter)
+    .attr("transform", `translate(0, ${innerHeight})`)
+    .call(xAxis)
+    .selectAll(".domain")
+    .remove();
+
+  const xAxisLabelText = xAxisGEnter
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("x", innerWidth / 2)
+    .attr("y", 75)
+    .attr("fill", "black")
+    // update the existing axis-label class
+    .merge(xAxisG.select(".axis-label"))
+    .text(xAxisLabel);
+  // x-axis end
+
+  // allows for circles to appear on the first invocation
+  const circles = g.merge(gEnter).selectAll("circle").data(data);
+
+  circles
+    .join("circle")
+    .attr("cy", (d) => yScale(yValue(d)))
+    .attr("cx", (d) => xScale(xValue(d)))
+    // .attr("width", (d) => xScale(d.population))
+    .attr("r", circleRadius);
+};
