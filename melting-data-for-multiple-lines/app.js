ad3def021b0d9610a7bbbeb4e67c4bef36642c1b
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-28 01:34:29 -0800

    refactored code to include general update pattern

--- melting-data-for-multiple-lines/app.js
+++ melting-data-for-multiple-lines/app.js
@@ -1,6 +1,9 @@
 import { loadAndProcessData, parseYear } from "./loadingAndProcessData.js";
 import { colorLegend } from "./colorLegend.js";
+import { lineChart } from "./lineChart.js";
 const svg = d3.select("svg");
+const lineChartG = svg.append("g");
+const colorLegendG = svg.append("g");
 
 // append method will append new DOM elements
 // will append the element into the svg element
@@ -10,90 +13,13 @@ const width = +svg.attr("width");
 
 const selectedYear = 2018;
 
-const render = (data) => {
-  const xValue = (d) => d.year;
-  const xAxisLabel = "Time";
+let data;
 
+const render = () => {
   const yValue = (d) => d.population;
-  const yAxisLabel = "Population";
-
   const colorValue = (d) => d.name;
-
-  const title = "Population Over Time By Region";
-  const margin = { top: 60, right: 280, bottom: 88, left: 105 };
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
-
-  const xScale = d3
-    .scaleTime()
-    // extent returns an array of max and min
-    .domain(d3.extent(data, xValue))
-    .range([0, innerWidth]);
-
-  const yScale = d3
-    .scaleLinear()
-    .domain(d3.extent(data, yValue))
-    .range([innerHeight, 0])
-    .nice();
-
   const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
-  const g = svg
-    .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-  const tickFormat = (number) =>
-    d3.format(".2s")(number).replace("G", "B").replace(".0", "");
-
-  const xAxis = d3
-    .axisBottom(xScale)
-    // .tickFormat(xAxisTickFormat)
-    .tickSize(-innerHeight)
-    .tickPadding(15);
-
-  const yAxis = d3
-    .axisLeft(yScale)
-    .tickSize(-innerWidth)
-    .tickPadding(10)
-    .tickFormat(tickFormat);
-  const yAxisG = g.append("g").call(yAxis);
-
-  yAxisG.selectAll(".domain").remove();
-
-  // bottom axis
-  const xAxisG = g
-    .append("g")
-    .call(xAxis)
-    .attr("transform", `translate(0, ${innerHeight})`);
-
-  xAxisG.select(".domain").remove();
-  // yAxis(g.append("g"));
-
-  yAxisG
-    .append("text")
-    .attr("class", "axis-label")
-    .attr("x", -innerHeight / 2)
-    .attr("y", -93)
-    .attr("transform", `rotate(-90)`)
-    .attr("fill", "black")
-    .attr("text-anchor", "middle")
-    .text(yAxisLabel);
-
-  xAxisG
-    .append("text")
-    .attr("class", "axis-label")
-    .attr("x", innerWidth / 2)
-    .attr("y", 75)
-    .text(xAxisLabel)
-    .attr("fill", "black");
-
-  const lineGenerator = d3
-    .line()
-    .x((d) => xScale(xValue(d)))
-    .y((d) => yScale(yValue(d)))
-    .curve(d3.curveBasis);
-
-  console.log(yValue);
   const lastYValue = (d) => yValue(d.values[d.values.length - 1]);
 
   const nested = d3
@@ -105,47 +31,36 @@ const render = (data) => {
 
   colorScale.domain(nested.map((d) => d.key));
 
-  // chart maker
-  g.selectAll(".line-path")
-    .data(nested)
-    .enter()
-    .append("path")
-    .attr("class", "line-path")
-    .attr("d", (d) => lineGenerator(d.values))
-    .attr("stroke", (d) => colorScale(d.key));
-
-  const selectedYearDate = parseYear(selectedYear);
-
-  // selectedYear
-  g.append("line")
-    .attr("class", "selected-year-line")
-    .attr("x1", xScale(selectedYearDate))
-    .attr("x2", xScale(selectedYearDate))
-    .attr("y1", 0)
-    .attr("y2", innerHeight)
-    .attr("stroke", "black");
-
-  g.append("text").attr("class", "title").attr("y", -10).text(title);
-
-  g.append("rect")
-    .attr("width", innerWidth)
-    .attr("height", innerHeight)
-    .attr("fill", "none")
-    .attr("pointer-events", "all")
-    .on("mousemove", () => {
-      const x = d3.mouse(g.node())[0];
-
-      // console.log(xScale.invert(x));
-      const hoveredDate = xScale.invert(x);
-      console.log(hoveredDate.getFullYear());
-    });
-
-  svg.append("g").attr("transform", `translate(735, 121)`).call(colorLegend, {
+  lineChartG.call(lineChart, {
     colorScale,
-    circleRadius: 10,
-    spacing: 55,
-    textOffset: 15,
+    colorValue,
+    yValue,
+
+    title: "Population Over Time By Region",
+    xValue: (d) => d.year,
+    xAxisLabel: "Time",
+    yAxisLabel: "Population",
+
+    margin: { top: 60, right: 280, bottom: 88, left: 105 },
+    width,
+    height,
+    data,
+    nested,
+    selectedYear,
   });
+
+  colorLegendG
+    .append("g")
+    .attr("transform", `translate(735, 121)`)
+    .call(colorLegend, {
+      colorScale,
+      circleRadius: 10,
+      spacing: 55,
+      textOffset: 15,
+    });
 };
 
-loadAndProcessData().then(render);
+loadAndProcessData().then((loadedData) => {
+  data = loadedData;
+  render();
+});
---
+++ melting-data-for-multiple-lines/lineChart.js
@@ -0,0 +1,146 @@
+import { parseYear } from "./loadingAndProcessData.js";
+export const lineChart = (selection, props) => {
+  const {
+    colorScale,
+    yValue,
+    xValue,
+    colorValue,
+    title,
+    xAxisLabel,
+    yAxisLabel,
+    margin,
+    width,
+    height,
+    data,
+    nested,
+    selectedYear,
+  } = props;
+
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  const xScale = d3
+    .scaleTime()
+    // extent returns an array of max and min
+    .domain(d3.extent(data, xValue))
+    .range([0, innerWidth]);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, yValue))
+    .range([innerHeight, 0])
+    .nice();
+
+  const g = selection.selectAll(".container").data([null]);
+  const gEnter = g.enter().append("g");
+  gEnter.merge(g).attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  const tickFormat = (number) =>
+    d3.format(".2s")(number).replace("G", "B").replace(".0", "");
+
+  const xAxis = d3
+    .axisBottom(xScale)
+    // .tickFormat(xAxisTickFormat)
+    .tickSize(-innerHeight)
+    .tickPadding(10);
+
+  const yAxis = d3
+    .axisLeft(yScale)
+    .tickSize(-innerWidth)
+    .tickPadding(10)
+    .tickFormat(tickFormat);
+
+  const yAxisGEnter = gEnter.append("g").attr("class", "y-axis");
+
+  const yAxisG = g.select(".y-axis");
+  yAxisGEnter
+    .merge(g.select(".y-axis"))
+    .call(yAxis)
+    .selectAll(".domain")
+    .remove();
+
+  yAxisGEnter
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("y", -93)
+    .attr("transform", `rotate(-90)`)
+    .attr("fill", "black")
+    .attr("text-anchor", "middle")
+    .merge(yAxisG.select(".axis-label"))
+    .attr("x", -innerHeight / 2)
+    .text(yAxisLabel);
+
+  // bottom axis
+  const xAxisGEnter = gEnter.append("g").attr("class", "x-axis");
+
+  const xAxisG = g.select(".x-axis");
+
+  xAxisGEnter
+    .merge(xAxisG)
+    .call(xAxis)
+    .attr("transform", `translate(0, ${innerHeight})`);
+
+  xAxisG.select(".domain").remove();
+  // yAxis(g.append("g"));
+
+  xAxisGEnter
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("fill", "black")
+    .attr("y", 75)
+    .merge(xAxisG.select(".axis-label"))
+    .attr("x", innerWidth / 2)
+    .text(xAxisLabel);
+
+  const lineGenerator = d3
+    .line()
+    .x((d) => xScale(xValue(d)))
+    .y((d) => yScale(yValue(d)))
+    .curve(d3.curveBasis);
+
+  // chart maker
+  const linePaths = g.merge(gEnter).selectAll(".line-path").data(nested);
+
+  linePaths
+    .enter()
+    .append("path")
+    .attr("class", "line-path")
+    .merge(linePaths)
+    .attr("d", (d) => lineGenerator(d.values))
+    .attr("stroke", (d) => colorScale(d.key));
+
+  const selectedYearDate = parseYear(selectedYear);
+
+  // selectedYear
+  gEnter
+    .append("line")
+    .attr("class", "selected-year-line")
+    .attr("y1", 0)
+    .merge(g.select(".selected-year-line"))
+    .attr("x1", xScale(selectedYearDate))
+    .attr("x2", xScale(selectedYearDate))
+    .attr("y2", innerHeight);
+
+  gEnter
+    .append("text")
+    .attr("class", "title")
+    .attr("y", -10)
+    .merge(g.select(".title"))
+    .text(title);
+
+  gEnter
+    .append("rect")
+    .attr("class", "mouse-interceptor")
+    .attr("width", innerWidth)
+    .attr("height", innerHeight)
+    .attr("fill", "none")
+    .attr("pointer-events", "all")
+    .merge(g.select(".mouse-interceptor"))
+    .on("mousemove", function () {
+      const x = d3.mouse(this)[0];
+
+      // console.log(xScale.invert(x));
+      const hoveredDate = xScale.invert(x);
+      console.log(hoveredDate.getFullYear());
+    });
+};
